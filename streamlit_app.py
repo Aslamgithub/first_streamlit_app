99495afba7ccc2a09e6328cac83cd3da2424d5a4
Aslam Raheem <72614275+Aslamgithub@users.noreply.github.com>
2023-09-19 17:04:41 +0530

    Update streamlit_app.py
    new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 
 streamlit.text(' 🥣 Omega3 & blueberry Oatmeal')
 streamlit.text('🥗 Kale')
 streamlit.text(' 🐔 Chicken')
 streamlit.text(' 🥑🍞 Avacado')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
